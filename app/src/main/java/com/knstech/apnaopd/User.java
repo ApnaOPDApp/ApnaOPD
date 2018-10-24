cbee75971e6c44ea4caaf7307cf642703ed552fe
Edward Vaghela <Lenovo@Edward.wizworld>
2018-10-25 02:18:49 +0530

    User account setup v1.1

--- app/src/main/java/com/knstech/apnaopd/User.java
+++ app/src/main/java/com/knstech/apnaopd/User.java
@@ -4,4 +4,20 @@ package com.knstech.apnaopd;
  * Created by Pankaj Vaghela on 25-10-2018.
  */
 public class User {
+    private String uid;
+    private String email;
+    private String name;
+    private String imageUrl;
+    private String gid;
+    private boolean isDoctor;
+    private boolean isRetailer;
+    private boolean isWholeseller;
+    private boolean isManufacturer;
+    private boolean isSupplier;
+
+    public User(){}
+
+
+    
+
 }
--- app/src/main/java/com/knstech/apnaopd/UserAuth.java
+++ app/src/main/java/com/knstech/apnaopd/UserAuth.java
@@ -1,7 +1,105 @@
 package com.knstech.apnaopd;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Pankaj Vaghela on 25-10-2018.
  */
 public class UserAuth {
+
+    private static User mUser;
+
+
+    public UserAuth(){}
+
+    public UserAuth getInstance(){
+         return ApnaOPDApp.getUserAuth();
+    }
+
+    public User getCurrentUser() {
+        return mUser;
+    }
+
+
+    public void signInGoogle(final Context context, final GoogleSignInAccount gAcc){
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String url ="http://www.google.com/api/users/"+gAcc.getId();
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response) {
+
+                        Toast.makeText(context, response, Toast.LENGTH_SHORT).show();
+
+                        if(response==null){
+                           signUpGoogle(context,gAcc);
+                        }else{
+
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+//                mTextView.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    private void signUpGoogle(final Context context, final GoogleSignInAccount gAcc){
+
+        String url ="http://www.google.com/api/users";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(context,response,Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(context,error.toString(),Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> user = new HashMap<String, String>();
+                user.put("gid", gAcc.getId());
+                user.put("name",gAcc.getDisplayName());
+                user.put("email",gAcc.getEmail());
+                user.put("imageUrl",gAcc.getPhotoUrl()+"");
+                return user;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
+    }
+
+
+
+
 }
