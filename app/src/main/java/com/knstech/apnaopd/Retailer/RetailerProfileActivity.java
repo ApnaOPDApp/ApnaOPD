28c0aaa387ba04b70fbe5a2af9d56f4af3047484
root <root@localhost.localdomain>
2018-11-14 16:12:37 +0530

    retailer profile

--- app/src/main/java/com/knstech/apnaopd/Retailer/RetailerProfileActivity.java
+++ app/src/main/java/com/knstech/apnaopd/Retailer/RetailerProfileActivity.java
@@ -1,16 +1,32 @@
 package com.knstech.apnaopd.Retailer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.knstech.apnaopd.R;
 
 public class RetailerProfileActivity extends AppCompatActivity {
 
+    private Button btn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retailer_profile);
+
+        btn = findViewById(R.id.edit_ret_profile);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RetailerProfileActivity.this,EditRetailerProfileActivity.class));
+
+            }
+        });
+
 
 
     }
